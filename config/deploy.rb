58ac7027c306937cc1bd22cb10d5bfe423dc1f34
@an280420 <angit280420@gmail.com>
2021-03-16 01:31:52 +0500

    Edit host, server

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.8.1'
 
 set :application, 'tubilinkz'
-set :repo_url, 'git@bitbucket.org:aristofun/tubilinkz.git'
+set :repo_url, 'git@github.com:an280420/tubilinkz.git'
 
 set :deploy_to, '/home/deploy/apps/tubilinkz'
 
